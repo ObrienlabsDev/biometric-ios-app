e27b195562d2d74716962f41d6454fd0ccebc131
Michael O'Brien <michael@obrienlabs.org>
2023-03-29 16:01:05 -0400

    #1 - initial ios app template

---
+++ biometric-ios/biometric-ios/biometric_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  biometric_iosApp.swift
+//  biometric-ios
+//
+//  Created by Michael O'Brien on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct biometric_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
